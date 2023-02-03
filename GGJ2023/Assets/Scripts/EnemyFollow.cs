73b43adfc5431ba7057d70c397e3002c7091809a
Caleb Tutor <cmtutor123@gmail.com>
2023-02-03 17:37:44 -0500

    began baseEnemy creation

---
+++ GGJ2023/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
