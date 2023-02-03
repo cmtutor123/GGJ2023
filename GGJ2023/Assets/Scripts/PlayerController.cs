58864119cabfaa98aa5147ac916793609e61b15d
Caleb Tutor <ctutor@uncc.edu>
2023-02-03 15:39:38 -0500

    directional sprite implemented

--- GGJ2023/Assets/Scripts/PlayerController.cs
+++ GGJ2023/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+    public Sprite[] directionalSprite;
+    public Camera cam;
+    public SpriteRenderer rend;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cam = GetComponentInChildren<Camera>();
+        rend = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
     {
+        // Movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         float tempSpeed = speed;
@@ -22,5 +28,19 @@ public class PlayerController : MonoBehaviour
             tempSpeed = speed / Mathf.Sqrt(2);
         }
         rb.velocity = new Vector2(horizontalInput * tempSpeed, verticalInput * tempSpeed);
+
+        // Determine Direction
+        Vector2 dir = Input.mousePosition - cam.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (angle < 0) angle += 360;
+        float angleIncrement = 360 / (directionalSprite.Length * 2);
+        int counter = 0;
+        while (angle - angleIncrement * counter > 0)
+        {
+            counter++;
+        }
+        int directionalSpriteIndex = counter / 2;
+        if (directionalSpriteIndex >= directionalSprite.Length) directionalSpriteIndex = 0;
+        rend.sprite = directionalSprite[directionalSpriteIndex];
     }
 }
