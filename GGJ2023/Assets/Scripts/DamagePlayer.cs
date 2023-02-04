7ad4b2096300d1f6908131f0ea311b53a8731e7b
Caleb Tutor <cmtutor123@gmail.com>
2023-02-04 08:18:48 -0500

    created player health system

---
+++ GGJ2023/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    GameObject player;
+
+    public int damage;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player") other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+    }
+}
---
+++ GGJ2023/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float invincibilityCooldown = 0;
+    public float invincibilityTime;
+    private int currentHealth;
+    public int startingHealth;
+
+    void PlayerDeath()
+    {
+        Debug.Log("Player Died");
+    }
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (invincibilityCooldown < 0)
+        {
+            invincibilityCooldown = invincibilityTime;
+            Debug.Log("Player Damaged");
+            currentHealth -= damage;
+            if (currentHealth <= 0)
+            {
+                PlayerDeath();
+            }
+        }
+    }
+
+    void Update()
+    {
+        invincibilityCooldown -= Time.deltaTime;
+    }
+}
